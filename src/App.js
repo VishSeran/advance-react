f8f0ce6e0b7196ac8bdbdd52e42cb950bba5f597
VishSeran <vishwa.sovis@gmail.com>
2026-01-26 12:49:08 +0530

    lab processing

--- src/App.js
+++ src/App.js
@@ -126,7 +126,7 @@ export default App; */
 
 //exercise to create a feedback form in little lemon resturant
 
-function App() {
+/* function App() {
 
   const [score, setScore] = useState("10")
   const [comment, setComment] = useState("");
@@ -172,6 +172,109 @@ function App() {
       </form>
     </div>
   )
+}
+
+export default App; */
+
+//import { validateEmail } from "./utils";
+
+const PasswordErrorMessage = () => {
+  return (
+    <p className="FieldError">Password should have at least 8 characters</p>
+  );
+};
+
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState({
+    value: "",
+    isTouched: false,
+  });
+  const [role, setRole] = useState("role");
+
+  const getIsFormValid = () => {
+    // Implement this function
+    return true;
+  };
+
+  const clearForm = () => {
+    // Implement this function
+  };
+
+  const handleSubmit = (e) => {
+
+    if (password.value.length > 8) {
+      PasswordErrorMessage();
+    } else {
+      alert("Account created!");
+      clearForm();
+      e.preventDefault();
+    }
+
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <h2>Sign Up</h2>
+          <div className="Field">
+            <label>
+              First name <sup>*</sup>
+            </label>
+            <input placeholder="First name" value={firstName} onChange={e => setFirstName(e.target.value)} />
+          </div>
+          <div className="Field">
+            <label>Last name</label>
+            <input placeholder="Last name" value={lastName} onChange={e => setLastName(e.target.value)} />
+          </div>
+          <div className="Field">
+            <label>
+              Email address <sup>*</sup>
+            </label>
+            <input placeholder="Email address" value={email} onChange={e => setEmail(e.target.value)} />
+          </div>
+          <div className="Field">
+            <label>
+              Password <sup>*</sup>
+            </label>
+            <input placeholder="Password" type="password" value={password.value} onChange={(e) => {
+              setPassword({
+                ...password,
+                value: e.target.value
+              })
+            }} onBlur={(e) => setPassword({
+              ...password,
+              isTouched: true
+            })
+            }
+            />
+            <div>
+              {password.value.length < 8 && (
+                <PasswordErrorMessage />
+              )}
+            </div>
+
+          </div>
+          <div className="Field">
+            <label>
+              Role <sup>*</sup>
+            </label>
+            <select value={role} onChange={e => setRole(e.target.value)}>
+              <option value="role">Role</option>
+              <option value="individual">Individual</option>
+              <option value="business">Business</option>
+            </select>
+          </div>
+          <button type="submit" disabled={!getIsFormValid()}>
+            Create account
+          </button>
+        </fieldset>
+      </form>
+    </div>
+  );
 }
 
 export default App;
