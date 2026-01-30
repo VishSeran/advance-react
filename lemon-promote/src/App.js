e817c62b4fa2f3478fb02017007f4bc3d79ad03a
VishSeran <vishwa.sovis@gmail.com>
2026-01-30 14:43:52 +0530

    lemon-promote completed

--- lemon-promote/src/App.js
+++ lemon-promote/src/App.js
@@ -1,8 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { RadioGroup, RadioOption } from './Radio';
 
 function App() {
- 
+
+    const [selected, setSelected] = useState("");
+
+    return (
+        <div className='App'>
+            <h1>How do you know about Little Lemon</h1>
+            <RadioGroup onChange={setSelected} selected={selected}>
+                <RadioOption value="social_media">Social Media</RadioOption>
+                <RadioOption value="friends">Friends</RadioOption>
+                <RadioOption value="advertising">Advertising</RadioOption>
+                <RadioOption value="other">Other</RadioOption>
+            </RadioGroup>
+            <button className='submit' disabled={!selected}>Submit</button>
+        </div>
+    )
 }
 
 export default App;
---
+++ lemon-promote/src/Radio/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Styles.css";
+
+export const RadioGroup = ({ onChange, selected, children }) => {
+
+    const RadioOption = React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+            onChange,
+            checked: child.props.value === selected
+        })
+    })
+
+    return <div className="RadioGroup">{RadioOption}</div>
+
+}
+
+
+
+export const RadioOption = ({ value, checked,onChange, children }) => {
+
+   
+
+    return(
+        <div className="RadioOption">
+            <input id={value} type="radio" name={value} checked={checked} onChange={(e) => {
+                onChange(e.target.value)
+            }} />
+            <label htmlFor= {value}>{children}</label>
+        </div>
+    )
+}
