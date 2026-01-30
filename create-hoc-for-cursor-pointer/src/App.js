bcf7d92459ef848279205f393d840c8a1888d4f3
VishSeran <vishwa.sovis@gmail.com>
2026-01-30 20:29:53 +0530

    hoc project done

--- create-hoc-for-cursor-pointer/src/App.js
+++ create-hoc-for-cursor-pointer/src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const withMousePosition = (WrappedComponent) => {
+
+  return (props) => {
+
+    const [mousePosition, setMousePosition] = useState({
+      x: 0,
+      y: 0
+    });
+
+    useEffect(() => {
+
+      const handleMousePositionChange = (e) => {
+
+        setMousePosition({
+          x: e.clientX,
+          y: e.clientY
+        });
+
+      }
+      window.addEventListener("mousemove", handleMousePositionChange);
+
+      return () => {
+        window.removeEventListener("mousemove", handleMousePositionChange);
+      }
+    }, []);
+
+    return <WrappedComponent {...props} mousePosition={mousePosition} />;
+  }
+}
+
+const PanelMouseLogger = ({ mousePosition }) => {
+  if (!mousePosition) return null;
+
+  return (
+    <div>
+      <h3>Mouse Position</h3>
+
+      <div className='paneldiv'>
+        <span className='panelSpan'>X: {mousePosition.x}</span>
+        <span className='panelSpan'>Y: {mousePosition.y}</span>
+      </div>
+    </div>
+  )
+}
+
+
+const PointMouseLogger = ({ mousePosition }) => {
+  if (!mousePosition) return null;
+
+  return (
+
+    <div>
+      <h3>Mouse Position</h3>
+      <p className='panelSpan'>
+        ({mousePosition.x}, {mousePosition.y});
+      </p>
+    </div>
+
+
+  )
+}
+
+const PanelMouseTracker = withMousePosition(PanelMouseLogger);
+const PointMouseTracker = withMousePosition(PointMouseLogger);
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Mouse Position Tracker</h1>
+
+      <PanelMouseTracker />
+      <PointMouseTracker />
     </div>
-  );
+  )
 }
 
 export default App;
